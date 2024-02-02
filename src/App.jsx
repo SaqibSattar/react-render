ae3a4f2342a0e80c46224f68b8f220d91b247faf
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-02 11:24:48 +0400

    Same Element Reference

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import UseReducer from "./components/UseReducer/UseReducer";
 import ObjectUseState from "./components/Immutable State/ObjectUseState";
 import ArrayUseState from "./components/Immutable State/ArrayUseState";
 import { Parent } from './components/Parent Child/Parent.jsx'
+import { ParentOne } from "./components/Optimization/ParentOne.jsx";
+import { ChildOne } from "./components/Optimization/ChildOne.jsx";
+import { GrandParent } from "./components/Optimization/GrandParent.jsx";
 
 function App() {
   return (
@@ -12,8 +15,12 @@ function App() {
       {/* <UseState /> 
       <UseReducer />
       <ObjectUseState />
-      <ArrayUseState /> */}
-      <Parent />
+      <ArrayUseState />
+      <Parent /> 
+      <ParentOne>
+        <ChildOne />
+      </ParentOne> */}
+      <GrandParent />
     </>
   );
 }
